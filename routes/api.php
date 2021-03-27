fbb3e825a9ad6cbc6fbc74d492d1019eba9eee38
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-03-27 22:59:39 +0600

    Rafsan's initial setup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,24 @@
 <?php
 
+Route::post('/login', function (\Illuminate\Http\Request $request)
+{
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        // Authentication passed...
+        $user = Auth::user();
+        $token = $user->createToken('myToken')->accessToken;
+
+        return response()->json($token);
+    }
+
+    return response('Email and Password Does Not Match!');
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::middleware('auth:api')->group(function () {
 
     Route::get('auth-user', 'AuthUserController@show');
